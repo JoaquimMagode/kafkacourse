bda82ae00b460f667836e3fa2ebc2da5e18a8987
Joaquim Magode <135075390+JoaquimMagode@users.noreply.github.com>
2026-01-20 14:46:23 +0530

    first commit

---
+++ src/main/java/com/kafkacourse/kafkacourse/KafkacourseApplication.java
@@ -0,0 +1,13 @@
+package com.kafkacourse.kafkacourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkacourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkacourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kafkacourse/kafkacourse/KafkacourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafkacourse.kafkacourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkacourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
